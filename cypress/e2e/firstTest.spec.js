95210a1b4e490850bee79a1b254d44ade7531a92
NancyHelena <nancy.helenam@gmail.com>
2023-09-24 21:38:17 -0300

    Lessons 23 and 24

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -60,4 +60,53 @@ describe('First test suite', ()=>{
             .find('nb-checkbox')
             .click()
     })
+    it('save subject of the command',()=>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain','Email')
+        cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain','Password')
+
+       // 1 Cypress Alias
+       cy.contains('nb-card', 'Using the Grid').as('usingTheGrid')
+       cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain','Email')
+       cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain','Password')
+
+       // 2 cypress then() methods
+       cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm=>{
+            cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain','Email')
+            cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain','Password') 
+        //  wrap is used to convert Jquery object to cypress
+       })
+    })
+
+    it.only('save subject of the command',()=>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        // 2
+        cy.get('[for="exampleInputEmail1"]').then( label =>{
+            const labelText = label.text()
+            expect(labelText).to.equal('Email address')
+            cy.wrap(labelText).should('contain', 'Email address')
+        })
+
+        //3 
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then( text=>{
+            expect(text).to.equal('Email address')
+        } )
+        cy.get('[for="exampleInputEmail1"]').invoke('text').as('labelText').should('contain','Email address')
+
+        //5 invoke property 
+        cy.get('#exampleInputEmail1').type('test@test.com')
+        cy.get('#exampleInputEmail1').invoke('prop','value').then(property =>{
+            expect(property).to.equal('test@test.com')
+        })
+
+    })
 })
